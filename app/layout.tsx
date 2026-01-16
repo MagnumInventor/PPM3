d28c839204940452dd9a7d33d562c62e42b839e1
Oleksandr Markovych <markoleks08@gmail.com>
2026-01-16 12:24:34 +0200

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Oleksandr Markovych portfolio',
+  description: 'Personal web-app created by Oleksandr Markovych a.k.a MagnumInventor on Github',
+  generator: 'OM',
   icons: {
     icon: [
       {
